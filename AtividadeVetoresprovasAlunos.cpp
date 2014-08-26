5210aab23ec76f9a6267dd83e609667764b2e257
Hewerson Freitas <hewerson.freitas@gmail.com>
2014-08-26 18:04:38 -0300

    Modificado geraNota(), adicionado exibirNotas

--- AtividadeVetoresprovasAlunos.cpp
+++ AtividadeVetoresprovasAlunos.cpp
@@ -1,13 +1,15 @@
 #include<stdlib.h>
 #include<stdio.h>
+#include <string.h>
 #include<iostream>
+#include <iomanip>
 
 using namespace std;
 
 //Assinatura de funções
 int lerOperacao(double notasAlunos[],int totalNotas);
 double recebeNotas(double notasAlunos[],int totalNotas);
-double exibirNotas(double notasAlunos, int totalNotas);
+double exibirNotas(double notasAlunos[], int totalNotas);
 double chamarMenu(double notasAlunos[], int totalNotas);
 double calculaMedia(double vetorNotas[],int totalNotas);
 void verificarNota(double vetorNotas[]);
@@ -15,8 +17,9 @@ void verificarNota(double vetorNotas[]);
 
 double geraNota()
 {
-    double notaRand = (double)(rand() % 10);
-    return notaRand;
+    double beforeRand = (double)(rand() % 10);
+    double afterRand = ((double)rand()/(double)RAND_MAX);
+    return beforeRand+afterRand;
 }
 
 double calculaMedia(double vetorNotas[],int totalNotas)
@@ -29,7 +32,8 @@ double calculaMedia(double vetorNotas[],int totalNotas)
     return mediaNotas/totalNotas;
 }
 
-void verificarNota(double vetorNotas[]){
+void verificarNota(double vetorNotas[])
+{
     int indiceNota;
     cout<<"Digite qual nota deseja buscar, sendo elas de 1 a 10: ";
     cin>>indiceNota;
@@ -37,7 +41,8 @@ void verificarNota(double vetorNotas[]){
 }
 
 int lerOperacao(double notasAlunos[],int totalNotas)
-{   int op;
+{
+    int op;
 
     cout << "Digite sua Opcao: ";
     cin >> op;
@@ -58,6 +63,9 @@ int lerOperacao(double notasAlunos[],int totalNotas)
         chamarMenu(notasAlunos,totalNotas);
         break;
     case 4:
+        exibirNotas(notasAlunos,totalNotas);
+        break;
+    case 5:
         cout<<"Saiu"<<endl;
         break;
     }
@@ -68,9 +76,18 @@ double recebeNotas(double notasAlunos[],int totalNotas)
     for(int i=0; i < totalNotas; i++)
     {
         notasAlunos[i] = geraNota();
-        cout << notasAlunos[i] <<", ";
+        cout << setiosflags (ios::fixed) << setprecision (2) <<"Av "<< i+1 << ":"<<notasAlunos[i] <<endl;//seta para duas casas decimais
     }
-    endl(cout);
+
+    return *notasAlunos;
+}
+
+double exibirNotas(double notasAlunos[],int totalNotas)
+{
+    for(int i=0; i < totalNotas; i++)
+    {
+        cout << setiosflags (ios::fixed) << setprecision (2) <<"Av "<< i+1 << ": "<<notasAlunos[i] <<endl;//seta para duas casas decimais
+    }
 
     return *notasAlunos;
 }
@@ -83,7 +100,8 @@ double chamarMenu(double notasAlunos[],int totalNotas)
     cout<<"[1] - Cadastrar Notas"<<endl;
     cout<<"[2] - Calcular Media"<<endl;
     cout<<"[3] - Verificar Nota Especifica"<<endl;
-    cout<<"[4] - Sair"<<endl;
+    cout<<"[4] - Exibir Todas as Notas"<<endl;
+    cout<<"[5] - Sair"<<endl;
     lerOperacao(notasAlunos,totalNotas);
 }
 
@@ -94,6 +112,7 @@ int main()
     double notasAlunos[totalNotas];
     chamarMenu(notasAlunos,totalNotas);
 
+    system("PAUSE");
     return 0;
 }
 //end main
